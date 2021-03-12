7ee1c132c2a66e04635ebf42ce6de6274e4704d3
jyldyz <jyldyz@jyldyzs-MacBook-Pro.local>
2021-03-12 15:45:17 +0600

    third

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './Game';
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Game />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
